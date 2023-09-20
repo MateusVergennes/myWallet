6489dea4ec2f462f08736b94f3ae0c4266d47867
MateusVergennes <mateusalpi@hotmail.com>
2023-09-20 14:27:38 -0400

    Developed the function to exit of the program and the _about program_ in tool bar and Menu Bar

--- mywallet.cpp
+++ mywallet.cpp
@@ -13,3 +13,16 @@ myWallet::~myWallet()
     delete ui;
 }
 
+
+void myWallet::on_actionSair_triggered(){
+    close();
+}
+
+
+void myWallet::on_actionSobre_N_s_triggered(){
+    QMessageBox::about(this, "Sobre Esse Projeto", "<h2>myWallet 1.0.0</h2>"
+                                                   "<p>Desenvolvido por Mateus Vergennes <br>"
+                                                   "Projeto de Carteira p/ Controle Financeiro de Qt com C++<br>"
+                                                   "Copyright 2008-2023 Matt Systems Inc. </p>");
+}
+
